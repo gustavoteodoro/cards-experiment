23b4903ae0af3e23e9bd3441410a861d0523d1d2
gustavotweak <gustavo@tweak.com>
2020-06-28 11:32:57 -0300

    test: Update TextField test

--- src/components/atoms/TextField/index.spec.ts
+++ src/components/atoms/TextField/index.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from "@vue/test-utils";
 import TextField from "./index.vue"
 
-describe("App", () => {
+describe("TextField", () => {
   it('renders correctly', () => {
     const wrapper = mount(TextField)
     expect(wrapper.element).toMatchSnapshot()

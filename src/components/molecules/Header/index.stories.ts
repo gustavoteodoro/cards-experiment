ed0fe081d318238eadc2b56d11623574cb856e6b
gustavotweak <gustavo@tweak.com>
2020-06-28 11:33:56 -0300

    docs: Header stories

---
+++ src/components/molecules/Header/index.stories.ts
@@ -0,0 +1,8 @@
+import Header from './index.vue';
+
+export default { title: 'Molecules/Header' };
+
+export const Default = () => ({
+  components: { Header },
+  template: '<Header title="Example Title" />'
+});

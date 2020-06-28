093a3aa648c3b50b315ee61cde7737a415af2a25
gustavotweak <gustavo@tweak.com>
2020-06-28 14:14:12 -0300

    docs: Button stories

---
+++ src/components/atoms/Button/index.stories.ts
@@ -0,0 +1,8 @@
+import Button from './index.vue';
+
+export default { title: 'Atoms/Button' };
+
+export const Default = () => ({
+  components: { Button },
+  template: '<Button label="Submit" />'
+});

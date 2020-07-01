2fc5670979a94bbc2e7595e47eb04a6dd91295f9
gustavotweak <gustavo@tweak.com>
2020-07-01 00:38:26 -0300

    feat: Deck page

---
+++ src/pages/Deck/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <Header :title="cardsLabel" />
+    <CardsForm />
+  </div>
+</template>
+
+<script>
+import Header from '../../components/molecules/Header/index.vue';
+import CardsForm from '../../components/organisms/CardsForm/index.vue';
+import { cardsLabel } from './data.json';
+
+export default {
+  name: 'Deck',
+  data() {
+    return {
+      cardsLabel,
+    };
+  },
+  props: {
+    msg: String,
+  },
+  components: {
+    Header,
+    CardsForm,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #e3f3f6;
+  padding: 0;
+  margin: 0;
+}
+</style>

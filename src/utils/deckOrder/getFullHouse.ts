4a29b2bfac846071b1efeb46b73d17a0a0ab923e
gustavotweak <gustavo@tweak.com>
2020-07-01 01:09:44 -0300

    fix: Compare only first letter ofcards to prevent a bug

--- src/utils/deckOrder/getFullHouse.ts
+++ src/utils/deckOrder/getFullHouse.ts
@@ -2,12 +2,13 @@
 export default ({ values, cards }) => {
   const combos = [];
   values.map(value => {
-    const found = cards.filter(card => card.value === value);
+    const found = cards.filter(card => card.value.charAt(0) === value);
     combos.push({ value, amount: found.length, cards: found });
   })
   const firstCombo = combos.find(combo => combo.amount >= 3);
   if (firstCombo) {
-    const secondCombo = combos.find(combo => combo.amount >= 2 && firstCombo.value !== combo.value);
+    const restFirstCombo = combos.filter(combo => firstCombo.value !== combo.value)
+    const secondCombo = restFirstCombo.find(combo => combo.amount >= 2);
     if (secondCombo) {
       const firstComboCards = firstCombo.cards.slice(0, 3);
       const secondComboCards = secondCombo.cards.slice(0, 2)

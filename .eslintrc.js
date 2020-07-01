e35887221f38e2d15be5821f6fa536a7438d1ef4
gustavotweak <gustavo@tweak.com>
2020-07-01 00:25:21 -0300

    chore: Updating eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': 0,
+    'import/first': 0,
   },
 };

4f4cb20e2e829c65c1e86a1720e178a89716cf5e
gustavotweak <gustavo@tweak.com>
2020-07-01 00:30:48 -0300

    setup: Vuex, vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+import Deck from './pages/Deck/index.vue';
+import NewDeck from './pages/NewDeck/index.vue';
+import store from './store/index.ts';
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
+const routes = [
+  { path: '/', component: Deck },
+  { path: '/new/:id', component: NewDeck },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
 new Vue({
+  router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');

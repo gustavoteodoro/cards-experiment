275b473a89a6ccc3cb034dc3d4f75e40c6d0f912
gustavotweak <gustavo@tweak.com>
2020-07-01 00:28:24 -0300

    feat: Create CardsPile organism

---
+++ src/components/organisms/CardsPile/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="cards-pile">
+    <Card
+      v-for="(card, index) in cards"
+      :key="`${card.suit + index}`"
+      :value="card.value"
+      :suit="card.suit"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import Card from '../../molecules/Card/index.vue';
+
+export default {
+  name: 'CardsForm',
+  components: {
+    Card,
+  },
+  computed: mapState({
+    cards: (state) => state.deck.cards,
+  }),
+};
+</script>
+
+<style scoped>
+.cards-pile {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 24px auto;
+  max-width: 1000px;
+  justify-content: flex-start;
+  background: #d8d8d8;
+  border: 2px solid #979797;
+  padding: 0 25px;
+}
+</style>
